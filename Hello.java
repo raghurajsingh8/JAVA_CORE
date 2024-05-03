062007314337d98cfa782674bf318675dfbc1de8
RAGHURAJ PRATAP SINGH <136258185+raghurajsingh8@users.noreply.github.com>
2024-05-03 23:33:43 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,23 @@
+class Hello{
+
+
+public static void main(String arg[]){
+
+System.out.println("Hello student kya haal chaal");
+
+
+}
+
+
+//body of the class
+//save Hello.ja
+//in terminal javac Hello.java
+//.class file genrate hoga
+//terminal me java Hello run kreneg 
+
+
+//all is done
+
+
+
+}
